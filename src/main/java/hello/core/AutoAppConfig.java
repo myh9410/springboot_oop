4ddfbee5cbe81eb88bab2257d82c68e8be9bedfc
myh9410 <mjskks94@naver.com>
2022-06-01 18:49:39 +0900

    빈 등록 시 충돌

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,7 @@
 package hello.core;
 
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -13,5 +15,10 @@ import org.springframework.context.annotation.FilterType;
 public class AutoAppConfig {
 
 
-
+    @Bean(name = "memoryMemberRepository")
+    //기존에는 수동 빈 등록 - 자동 빈 등록 이름이 같다면 override한다
+    //최근 스프링은 spring.main.allow-bean-definition-overriding=true 인경우만 override, false이면 에러 발생하도록 변경됨
+    MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }

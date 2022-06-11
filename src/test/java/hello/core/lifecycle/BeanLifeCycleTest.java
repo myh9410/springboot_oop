0b370a74daa65a46aab9e7d5cc95de1982ea3347
myh9410 <mjskks94@naver.com>
2022-06-11 19:23:49 +0900

    @PostConstruct, @PreDestroy

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -18,10 +18,11 @@ public class BeanLifeCycleTest {
     @Configuration
     static class LifeCycleConfig {
 
-        @Bean(initMethod = "init", destroyMethod = "close")
+        // @Bean(initMethod = "init", destroyMethod = "close")
         // destroyMethod의 default값은 inferred(추론)
         // inferred는 close, shutdown이라는 이름의 메서드를 자동 호출한다.
         // 추론 기능을 사용하기 싫다면 destroyMethod = ""로 설정
+        @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,6 +3,9 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -32,12 +35,17 @@ public class NetworkClient {
     }
 
 
+    // implementation이나 @Bean(init, destroy)보다
+    // 최신 스프링에서 권장하는 @PostConstruct, @PreDestroy 를 사용한다 - 자바 표준이라 스프링 아니어도 동작한다.
+    // 단, 외부 라이브러리에서는 사용 못해서 외부 라이브러리에서는 @Bean을 사용해야한다.
+    @PostConstruct
     public void init() throws Exception {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
 
+    @PreDestroy
     public void close() throws Exception {
         System.out.println("NetworkClient.close");
         disconnect();
